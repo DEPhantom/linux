c3cb0ee5122cec88259377e9482fe258b472e31c
DEPhantom <55129236+DEPhantom@users.noreply.github.com>
2021-07-20 00:27:58 +0800

    Add files via upload

---
+++ Bidder3.py
@@ -0,0 +1,92 @@
+#!/usr/bin/python
+
+import rospy
+from std_msgs.msg import String
+from std_msgs.msg import Int16
+from random import randint
+
+end = False
+money = "100"
+limit = "300"
+
+pub = rospy.Publisher( "join", String, queue_size = 10 )
+pub2 = rospy.Publisher( "bid3", Int16, queue_size = 10 )
+
+def check_price( data ):
+  global money
+  money = data.data
+  end = True
+# end check_price()
+
+def standby():
+  global pub
+  pub.publish( "I'm ready" )
+
+# end standby()
+
+def ready():
+  global end
+  rospy.Subsciber( "join3", String, check_price )
+  timer = rospy.Timer( rospy.Duration( 0.5 ), standby )
+  while( end == False ):
+    rospy.sleep(1)
+  # end while()
+  
+  timer.shutdown()
+  
+# end ready()
+
+def add_price( data ):
+  global winner, money, limit, end
+  
+  if ( data.data > money and data.data < limit ):
+    money = data.data+randint( 1, 10 )
+	rospy.loginfo( "Bidder3: %d" %money )
+    pub2.publish( money )
+  # end if
+  
+# end add_price
+
+def start():
+  global end, winner, money, pub
+  end = False
+  money = money+randint( 1, 10 )
+  rospy.loginfo( "Bidder3: %d" %money )
+  pub2.publish( money )
+  rospy.Subsciber( "bid3", Int16, add_price )
+  while( not end ):
+    rospy.sleep( 0.001 )
+  # end while()
+  
+# end start()
+
+def main():
+  rospy.init_node( "Bidder3", anonymous = True )
+  ready()
+  start()
+  
+# end main()
+
+if __name__ == "__main__":
+  main()
+# end if
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Tenderer.py
@@ -0,0 +1,141 @@
+#!/usr/bin/python
+
+import rospy
+from std_msgs.msg import String
+from std_msgs.msg import Int16
+
+has_join1 = False
+has_join2 = False
+has_join3 = False
+end = False
+money = "100"
+target = "300"
+winner = ""
+
+pub = rospy.Publisher( "price", Int16, queue_size = 10 )
+
+def check1( data ):
+  global has_join1
+  rospy.loginfo( "Bidder1 is join" )
+  has_join1 = True
+# end check1()
+
+def check2( data ):
+  global has_join2
+  rospy.loginfo( "Bidder2 is join" )
+  has_join2 = True
+# end check2()
+
+def check3( data ):
+  global has_join3
+  rospy.loginfo( "Bidder3 is join" )
+  has_join3 = True
+# end check3()
+
+def ready():
+  global has_join1, has_join2, has_join3
+  rospy.Subsciber( "join1", String, check1 )
+  rospy.Subsciber( "join2", String, check2 )
+  rospy.Subsciber( "join3", String, check3 )
+  while( has_join1 == False or has_join2 == False or has_join3 == False ):
+    rospy.sleep(1)
+  # end while()
+  
+  rospy.loginfo( "Now start bidding" )
+  
+# end ready()
+
+def check_num1( data ):
+  global winner, money, target, end
+  
+  if ( data.data > money ):
+    money = data.data
+	rospy.loginfo( "Leader is bidder1, price is %d" %money )
+	pub.publish( money )
+  # end if
+  
+  if ( money >= target ):
+    end = True
+	winner = "bidder1"
+  # end if
+  
+# end check_num1
+
+def check_num2( data ):
+  global winner, money, target, end
+  
+  if ( data.data > money ):
+    money = data.data
+	rospy.loginfo( "Leader is bidder2, price is %d" %money )
+	pub.publish( money )
+  # end if
+  
+  if ( money >= target ):
+    end = True
+	winner = "bidder2"
+  # end if
+  
+# end check_num2
+
+def check_num3( data ):
+  global winner, money, target, end
+  
+  if ( data.data > money ):
+    money = data.data
+	rospy.loginfo( "Leader is bidder3, price is %d" %money )
+	pub.publish( money )
+  # end if
+  
+  if ( money >= target ):
+    end = True
+	winner = "bidder3"
+  # end if
+  
+# end check_num3
+
+def start():
+  global end, winner, money, pub
+  end = False
+  rospy.loginfo( "start from %d" %money )
+  pub.publish( money )
+  rospy.Subsciber( "bid1", Int16, check_num1 )
+  rospy.Subsciber( "bid2", Int16, check_num2 )
+  rospy.Subsciber( "bid3", Int16, check_num3 )
+  while( not end ):
+    rospy.sleep( 0.001 )
+  # end while()
+  
+  rospy.loginfo( "%s use %d get the item" %( winner, money ) )
+  
+# end start()
+
+def main():
+  rospy.init_node( "Tenderer", anonymous = True )
+  ready()
+  start()
+  
+# end main()
+
+if __name__ == "__main__":
+  main()
+# end if
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
